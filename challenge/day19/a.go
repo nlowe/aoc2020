4c712a8df8904ecc3b886105041c2d011c58aa4c
Nathan Lowe <public@nlowe.me>
2021-12-05 01:49:48 -0800

    Re-write 19 to make part b actually do-able. Still super-hack-y

--- challenge/day19/a.go
+++ challenge/day19/a.go
@@ -2,6 +2,7 @@ package day19
 
 import (
 	"fmt"
+	"regexp"
 	"strings"
 
 	"github.com/nlowe/aoc2020/util"
@@ -20,145 +21,63 @@ func aCommand() *cobra.Command {
 	}
 }
 
-type Rule interface {
-	// Match returns the remaining portion of the string and whether or not the rule matched
-	Match(string) (string, bool)
-	// Link un-refs rules once all rules are known
-	Link(map[int]Rule)
-}
+type rule string
 
-// literal is a Rule that matches a single rune
-type literal rune
-
-func (l literal) Match(s string) (string, bool) {
-	if len(s) >= 1 && rune(s[0]) == rune(l) {
-		return s[1:], true
-	}
-
-	return s, false
-}
-func (l literal) Link(map[int]Rule) {}
-
-// or is a Rule that matches when either of its sub rules are matched
-type or struct {
-	left  Rule
-	right Rule
-}
-
-func (o or) Match(s string) (string, bool) {
-	if remaining, match := o.left.Match(s); match {
-		return remaining, true
-	} else if remaining, match := o.right.Match(s); match {
-		return remaining, true
-	}
-
-	return s, false
-}
-func (o or) Link(rules map[int]Rule) {
-	o.left.Link(rules)
-	o.right.Link(rules)
-}
-
-// sequence is a Rule that matches if all rules in the sequence match in order
-type sequence []Rule
-
-func (s sequence) Match(str string) (string, bool) {
-	remaining := str
-	match := false
-	for _, rule := range s {
-		remaining, match = rule.Match(remaining)
-		if !match {
-			return str, false
+func (r rule) toRegex(rules map[int]rule) string {
+	switch {
+	case r == `"a"`:
+		return "a"
+	case r == `"b"`:
+		return "b"
+	case strings.Contains(string(r), "|"):
+		result := strings.Builder{}
+		for _, nested := range strings.Split(string(r), "|") {
+			result.WriteString("(?:")
+			result.WriteString(rule(nested).toRegex(rules))
+			result.WriteString(")|")
 		}
+
+		return strings.TrimRight(result.String(), "|")
+	case strings.Contains(string(r), " "):
+		result := strings.Builder{}
+		for _, seq := range strings.Fields(string(r)) {
+			result.WriteString("(?:")
+			result.WriteString(rule(seq).toRegex(rules))
+			result.WriteString(")")
+		}
+
+		return result.String()
+	default:
+		return rules[util.MustAtoI(string(r))].toRegex(rules)
 	}
-
-	return remaining, true
-}
-func (s sequence) Link(rules map[int]Rule) {
-	for _, r := range s {
-		r.Link(rules)
-	}
-}
-
-// ref is a Rule that references another rule in the rule set
-type ref struct {
-	n   int
-	ref Rule
-}
-
-func (r ref) Match(s string) (string, bool) {
-	if r.ref == nil {
-		panic(fmt.Errorf("reference rule %d was not linked", r.n))
-	}
-
-	return r.ref.Match(s)
-}
-func (r *ref) Link(rules map[int]Rule) {
-	r.ref = rules[r.n]
 }
 
 func partA(challenge *challenge.Input) int {
-	rules := map[int]Rule{}
+	rules := map[int]rule{}
 
 	buildingRules := true
 	matches := 0
+
+	var regex *regexp.Regexp
 
 	for line := range challenge.Lines() {
 		if line == "" {
 			buildingRules = false
 
-			// Link all ref rules
-			for _, r := range rules {
-				r.Link(rules)
-			}
+			regex = regexp.MustCompile("^" + rules[0].toRegex(rules) + "$")
 
 			continue
 		}
 
 		if buildingRules {
-			n, r := parse(line)
-			rules[n] = r
-		} else {
-			if left, match := rules[0].Match(line); match && left == "" {
+			parts := strings.Split(line, ": ")
+			rules[util.MustAtoI(parts[0])] = rule(parts[1])
+		} else if line != "" && regex != nil {
+			if regex.Match([]byte(line)) {
 				matches++
 			}
 		}
 	}
 
 	return matches
-}
-
-func parse(line string) (int, Rule) {
-	parts := strings.Split(line, ":")
-
-	n := util.MustAtoI(parts[0])
-
-	rawRule := strings.TrimSpace(parts[1])
-
-	if strings.Contains(rawRule, "|") {
-		parts = strings.Split(rawRule, "|")
-
-		return n, or{
-			left:  parseSequence(parts[0]),
-			right: parseSequence(parts[1]),
-		}
-	} else if rawRule == `"a"` {
-		return n, literal('a')
-	} else if rawRule == `"b"` {
-		return n, literal('b')
-	} else {
-		// It's a sequence
-		return n, parseSequence(rawRule)
-	}
-}
-
-func parseSequence(rawRule string) sequence {
-	rules := strings.Fields(rawRule)
-
-	var seq sequence
-	for _, rule := range rules {
-		seq = append(seq, &ref{n: util.MustAtoI(rule)})
-	}
-
-	return seq
 }
--- challenge/day19/b.go
+++ challenge/day19/b.go
@@ -2,10 +2,16 @@ package day19
 
 import (
 	"fmt"
+	"regexp"
+	"strings"
+
+	"github.com/nlowe/aoc2020/util"
 
 	"github.com/nlowe/aoc2020/challenge"
 	"github.com/spf13/cobra"
 )
+
+const maxDepth = 10
 
 func bCommand() *cobra.Command {
 	return &cobra.Command{
@@ -18,5 +24,48 @@ func bCommand() *cobra.Command {
 }
 
 func partB(challenge *challenge.Input) int {
-	panic("Not implemented!")
+	rules := map[int]rule{}
+
+	buildingRules := true
+	matches := 0
+
+	var regex *regexp.Regexp
+
+	for line := range challenge.Lines() {
+		if line == "" {
+			buildingRules = false
+
+			rules[8] = rule(strings.Join(hack(func(i int) string {
+				return strings.Repeat("42 ", i)
+			}), "|"))
+			rules[11] = rule(strings.Join(hack(func(i int) string {
+				return strings.Repeat("42 ", i) + " " + strings.Repeat("31 ", i)
+			}), "|"))
+
+			regex = regexp.MustCompile("^" + rules[0].toRegex(rules) + "$")
+
+			continue
+		}
+
+		if buildingRules {
+			parts := strings.Split(line, ": ")
+			rules[util.MustAtoI(parts[0])] = rule(parts[1])
+		} else if line != "" && regex != nil {
+			if regex.Match([]byte(line)) {
+				matches++
+			}
+		}
+	}
+
+	return matches
+}
+
+func hack(gen func(int) string) []string {
+	result := make([]string, maxDepth-1)
+
+	for i := 1; i < maxDepth; i++ {
+		result[i-1] = gen(i)
+	}
+
+	return result
 }
